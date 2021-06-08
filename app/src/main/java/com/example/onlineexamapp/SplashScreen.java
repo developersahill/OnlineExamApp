b46cca15bc230124db6e90b08bcc8ebfc20367f3
Mitul Patel <mitul71101@gmail.com>
2021-06-08 15:59:45 +0530

    Login & Signup UI

--- app/src/main/java/com/example/onlineexamapp/SplashScreen.java
+++ app/src/main/java/com/example/onlineexamapp/SplashScreen.java
@@ -5,18 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    LottieAnimationView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        animationView = findViewById(R.id.splashimage);
+
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent splashintent=new Intent(SplashScreen.this,StudentsMain.class);
+
+                animationView.playAnimation();
+                Intent splashintent=new Intent(SplashScreen.this,LoginActivity.class);
                 startActivity(splashintent);
                 finish();
             }
